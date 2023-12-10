466f3cd5b1482cc64b17dc09745535ef6df0812a
soul <104170577@qq.com>
2023-12-10 21:59:20 +0800

    feat: carousel

--- vanilla/carousel/main.js
+++ vanilla/carousel/main.js
@@ -1,1 +1,36 @@
+const nextBtn = document.getElementsByClassName("next-btn")[0];
+const prevBtn = document.getElementsByClassName("prev-btn")[0];
+const container = document.getElementsByClassName("container")[0];
 
+function push() {
+  const firstImg = container.firstElementChild;
+
+  container.appendChild(firstImg.cloneNode(true));
+  firstImg.remove();
+}
+
+function toRight() {
+  if (container.scrollLeft === container.scrollWidth - container.clientWidth) {
+    push();
+  }
+
+  setTimeout(() => {
+    container.scrollLeft += container.clientWidth;
+  }, 50);
+}
+
+function toLeft() {
+  container.scrollLeft -= container.clientWidth;
+}
+
+nextBtn.addEventListener("click", () => {
+  toRight();
+});
+
+prevBtn.addEventListener("click", () => {
+  toLeft();
+});
+
+setInterval(() => {
+  toRight();
+}, 2000);
